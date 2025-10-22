d975d4a43107f2d17e960d3817027e051d7ea241
Taylanne <taylannecastelobrancocavalcant@gmail.com>
2025-10-22 20:33:38 -0300

    codigo

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,35 +1,66 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
-import './App.css'
-import HomePage from './pages/HomePage'
-import ProfilePage from './pages/ProfilePage'
-import FilterContextProvider from './contexts/filters/FilterContext'
-import CardExpandidoProvider from './contexts/main/CardExpandidoContext'
-import InstituicaoAtualProvider from './contexts/main/InstituicaoAtualContext'
+import { useState, useEffect } from 'react';
+import { Navigate, Route, Routes, Link } from 'react-router-dom';
+import './App.css';
+import HomePage from './pages/HomePage';
+import ProfilePage from './pages/ProfilePage';
+import FilterContextProvider from './contexts/filters/FilterContext';
+import CardExpandidoProvider from './contexts/main/CardExpandidoContext';
+import InstituicaoAtualProvider from './contexts/main/InstituicaoAtualContext';
+import logo from './assets/Logo.png';
+import { FaSearch } from 'react-icons/fa';
+
 
 function App() {
+  const [showTopBar, setShowTopBar] = useState(true);
+
+  useEffect(() => {
+    let lastScroll = 0;
+    const handleScroll = () => {
+      const currentScroll = window.scrollY;
+      if (currentScroll > lastScroll) setShowTopBar(false);
+      else setShowTopBar(true);
+      lastScroll = currentScroll;
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
   return (
     <>
+      <div className={`top-bar ${showTopBar ? 'visible' : 'hidden'}`}>
+        {}
+        <Link to="/home">
+          <img src={logo} className="top-bar-logo" />
+        </Link>
+
+        {}
+          <div className="search-bar">
+          <FaSearch className="search-icon" />
+          <input
+            type="text"
+            placeholder="Pesquisar..."
+            className="search-input"
+          />
+        </div>
+
+      </div>
+
       <FilterContextProvider>
-
         <InstituicaoAtualProvider>
-
           <CardExpandidoProvider>
-
-            <Routes>
-              <Route path='/' element={<Navigate to={'/home'}/>}/>
-              <Route path='/home' element={<HomePage />}/>
-              <Route path='/profile' element={<ProfilePage />}/>
-            </Routes>
-
+            <div className="app-content">
+              <Routes>
+                <Route path="/" element={<Navigate to={'/home'} />} />
+                <Route path="/home" element={<HomePage />} />
+                <Route path="/profile" element={<ProfilePage />} />
+              </Routes>
+            </div>
           </CardExpandidoProvider>
-
         </InstituicaoAtualProvider>
-
       </FilterContextProvider>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
